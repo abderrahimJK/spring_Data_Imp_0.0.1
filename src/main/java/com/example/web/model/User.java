6611219f1d373e30b95602cdd2dd392bcfc22845
abderrahimJK <abderrahim.aitbouna@gmail.com>
2022-03-24 17:09:57 +0100

    add getters/setters to our entitys

--- src/main/java/com/example/web/model/User.java
+++ src/main/java/com/example/web/model/User.java
@@ -21,5 +21,40 @@ public class User {
 	
 	//
 	@OneToMany(mappedBy = "user")
-	private Collection<User> users;
+	private Collection<UserProduct> buys;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Collection<UserProduct> getBuys() {
+		return buys;
+	}
+
+	public void setBuys(Collection<UserProduct> buys) {
+		this.buys = buys;
+	}
+	
+	
+	
 }
--- src/main/java/com/example/web/model/UserProduct.java
+++ src/main/java/com/example/web/model/UserProduct.java
@@ -1,9 +1,10 @@
 package com.example.web.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 
@@ -12,6 +13,9 @@ public class UserProduct {
 
 	@EmbeddedId
 	UserProductKey id;
+	
+	private LocalDateTime buyDate;
+	private int amount;
 	
 	//
 	@ManyToOne
@@ -23,5 +27,44 @@ public class UserProduct {
 	@MapsId("productId")
 	@JoinColumn(name="product_id")
 	private Product product;
+
+	public UserProductKey getId() {
+		return id;
+	}
+
+	public LocalDateTime getBuyDate() {
+		return buyDate;
+	}
+
+	public void setBuyDate(LocalDateTime buyDate) {
+		this.buyDate = buyDate;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	
+	
 	
 }
